64a296cf635a7992424d9f1f19d9c33713f98465
nagi_py <95587372+nagipy@users.noreply.github.com>
2023-11-20 16:31:25 +0900

    index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({
+  intents: [
+    GatwayIntentBits.Guidls,
+    GatwayIntentBits.GuildVoiceStates,
+    GatwayIntentBits.GuildMembers
+  ]
+})
+
+const logsPath = path.join(__dirname, "logs");
+const logsFiles = fs.reddirSync(logsPath).filter(file => file.endsWith(".js"));
+
+for (const file of logsFiles) {
+  const log = require(`./logs/${file}`);
+
+  client.on("ready", () => { log(client) });
+}
+
+client.on("ready", () => {
+  client.user.setStatus("idle")
+
+  console.log(client.user.tag)
+})
+
+client.login(token)
